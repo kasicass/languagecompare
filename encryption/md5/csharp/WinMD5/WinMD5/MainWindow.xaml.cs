758d85af7145682ef806d8c16ce22c3867212572
kasicass <kasicass@gmail.com>
2016-07-19 16:11:37 +0800

    WinMD5 fun~

---
+++ encryption/md5/csharp/WinMD5/WinMD5/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace WinMD5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string BytesToHexString(byte[] data)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                this.txtCurrent.Text = files[0];
+
+                MD5 m = MD5.Create();
+                byte[] hashValue = m.ComputeHash(new FileStream(files[0], FileMode.Open));
+                this.txtResult.Text = BytesToHexString(hashValue);
+            }
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
