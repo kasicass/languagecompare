df1fc1ebb681c3abb48fc8df2d86dc6b81523216
kasicass <kasicass@gmail.com>
2016-07-21 15:56:10 +0800

    WinMD5 - compute md5 in background thread

--- encryption/md5/csharp/WinMD5/WinMD5/MainWindow.xaml.cs
+++ encryption/md5/csharp/WinMD5/WinMD5/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using System.Text;
+using System.Windows;
+using System.ComponentModel;
 using System.Security.Cryptography;
 
 namespace WinMD5
@@ -23,6 +12,8 @@ namespace WinMD5
     /// </summary>
     public partial class MainWindow : Window
     {
+        BackgroundWorker _worker;
+
         public static string BytesToHexString(byte[] data)
         {
             StringBuilder builder = new StringBuilder();
@@ -33,21 +24,64 @@ namespace WinMD5
             return builder.ToString();
         }
 
+        public void ComputeMD5_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bw = sender as BackgroundWorker;
+            string file = (string)e.Argument;
+
+            MD5 m = MD5.Create();
+            using (var fs = new FileStream(file, FileMode.Open))
+            {
+                byte[] hashValue = m.ComputeHash(fs);
+                e.Result = BytesToHexString(hashValue) + "    " + Path.GetFileName(file);
+            }
+        }
+
+        public void ComputeMD5_ReportProgress(object sender, ProgressChangedEventArgs e)
+        {
+        }
+
+        public void ComputeMD5_Completed(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("Cancelled");
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("Err: " + e.Error.Message);
+            }
+            else
+            {
+                string md5hex = (string)e.Result;
+                this.txtResult.Text = md5hex;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _worker = new BackgroundWorker();
+            _worker.WorkerReportsProgress = true;
+            _worker.DoWork += this.ComputeMD5_DoWork;
+            _worker.ProgressChanged += this.ComputeMD5_ReportProgress;
+            _worker.RunWorkerCompleted += this.ComputeMD5_Completed;
         }
 
         private void Window_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
+                if (_worker.IsBusy)
+                {
+                    return;
+                }
+
                 string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
                 this.txtCurrent.Text = files[0];
 
-                MD5 m = MD5.Create();
-                byte[] hashValue = m.ComputeHash(new FileStream(files[0], FileMode.Open));
-                this.txtResult.Text = BytesToHexString(hashValue);
+                _worker.RunWorkerAsync(files[0]);
             }
         }
 
